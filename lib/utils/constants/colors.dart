87f175a18091d9b46abe22f126e59256d8bb7410
mdmamunurrashidhridoy <rashid15-5273@diu.edu.bd>
2025-03-09 10:22:07 +0600

    E-Commerce Replica

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FColors {
+  static const Color primaryBlueOcean = Color(0xFF5CC936);
+  static const Color primaryOrangeFox = Color(0xFFFFC120);
+  static const Color primaryPureWhite = Color(0xFFFFFFFF);
+  static const Color primaryNavyBlack = Color(0xFF0C1A30);
+
+  static const Color secondaryEarthGreen = Color(0xFF3A9B7A);
+  static const Color secondaryRedVelvet = Color(0xFFFE3A30);
+  static const Color secondaryDarkGrey = Color(0xFF838589);
+  static const Color secondaryHalfGrey = Color(0xFFC4C5C4);
+  static const Color secondarySoftGrey = Color(0xFFEDEDED);
+  static const Color secondaryOfffGrey = Color(0xFFFAFAFA);
+
+  static const Color labelOffBlue = Color(0xFFEFF5FB);
+  static const Color labelOffGreen = Color(0xFFEEFAF6);
+  static const Color labelOffRed = Color(0xFFFCECEF);
+}
