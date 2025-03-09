d43d6108410636077c93089ce3f2f148c9d470de
mdmamunurrashidhridoy <rashid15-5273@diu.edu.bd>
2025-03-09 10:45:13 +0600

    font Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:ecommerce_replica/screens/home/pages/home.dart';
 
 void main() {
-  runApp();
+  runApp(MyEcom());
 }
 
+class MyEcom extends StatelessWidget {
+  const MyEcom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Ecom',
+      home: Home(),
+    );
+  }
+}
