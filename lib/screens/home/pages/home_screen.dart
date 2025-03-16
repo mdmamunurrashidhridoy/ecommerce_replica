3d4d3a238c31aac41f6a851829a9aedcddc50c8b
mdmamunurrashidhridoy <rashid115-5273@diu.edu.bd>
2025-03-16 11:30:09 +0600

    error code

--- lib/screens/home/pages/home_screen.dart
+++ lib/screens/home/pages/home_screen.dart
@@ -1,8 +1,15 @@
+import 'dart:convert';
+
+import 'package:ecommerce_replica/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
+import 'package:http/http.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
+  Future fetchData() async {
+    var response = await http.get(Uri.parse('https://dummyjson.com/products'))
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +21,35 @@ class HomeScreen extends StatelessWidget {
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Image.network('https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg'),
-            Text("Nike Shoe", style: TextStyle(
-              fontSize: 20,
-              fontFamily: 'DMSans',
-              
-            ),),
-            Text("\$5500"),
-            Text('\$4500'),
+            Image.network(
+                'https://d2v5dzhdg4zhx3.cloudfront.net/web-assets/images/storypages/primary/ProductShowcasesampleimages/JPEG/Product+Showcase-1.jpg',
+                ),
+            Text(
+              "Nike Shoe",
+              style: TextStyle(
+                  fontSize: 25,
+                  
+                  fontFamily: 'DMSans',
+                  fontWeight: FontWeight.w500),
+            ),
+            Text(
+              "\$5500",
+              style: TextStyle(
+                  fontSize: 20,
+                  color: FColors.secondaryRed,
+                  fontFamily: 'DMSans',
+                  fontWeight: FontWeight.w500),
+            ),
+            Text(
+              '\$6500',
+              style: TextStyle(
+                  fontSize: 17,
+                  fontFamily: 'DMSans',
+                  
+                  fontWeight: FontWeight.bold),
+            ),
             Row(
               children: [
                 Icon(FontAwesome.star_solid),
